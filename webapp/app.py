028a2a241b19ac3c016cb38706a41c4496f0e8e3
sai meghana muthyala <smuthyal@buffalo.edu>
2023-04-07 20:12:20 -0400

    UI changes

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_file,jsonify
+from subprocess import Popen,call
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Get a list of the filenames in the "uploads" folder
+    filenames = os.listdir('uploads/video')
+    return render_template('index.html', filenames=filenames)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -24,12 +30,34 @@ def upload():
         return render_template('index.html', message='Please upload files with allowed formats: mp3, wav, mp4, avi')
 
     # save files to separate folders
-    audio_file.save(f'uploads/{audio_file.filename}')
-    video_file.save(f'uploads/{video_file.filename}')
+    audio_file.save(f'uploads/audio/{audio_file.filename}')
+    video_file.save(f'uploads/video/{video_file.filename}')
 
-    return render_template('index.html', message='Files uploaded successfully')
+    return redirect(url_for('list_videos'))
+
+@app.route('/list_videos')
+def list_videos():
+    # Get a list of the filenames in the "uploads/video" folder
+    filenames = os.listdir('uploads/video/')
+    return render_template('list_videos.html', filenames=filenames)
+
+@app.route('/play/<filename>')
+def play(filename):
+    # Check if the file exists
+    print(filename)
+    if not os.path.isfile('uploads/video/' + filename):
+        return "File not found"
+
+    # Play the video using ffplay
+    call(['ffplay', '-i', 'uploads/video/' + filename])
+
+    return ''
+
+@app.route('/clanText')
+def get_clan_text():
+    response = requests.get('http://localhost:3028/clanText')
+    data = response.content
+    return render_template('clan_text.html', data=data.decode())
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
